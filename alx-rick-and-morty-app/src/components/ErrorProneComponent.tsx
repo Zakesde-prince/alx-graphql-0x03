69805376ac00854c994bd49721c146fe8d9fd7a0
Zakhele <prince.feela2905@gmail.com>
2025-08-21 01:55:55 +0200

    .

--- alx-rick-and-morty-app/src/components/ErrorProneComponent.tsx
+++ alx-rick-and-morty-app/src/components/ErrorProneComponent.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ErrorProneComponent: React.FC = () => {
-  // Remove or comment out the test error
-  // throw new Error("This is a test error!");
+  const [throwError, setThrowError] = useState(false);
 
-  return <div>Hello! Component is rendering correctly.</div>;
+  // Only throw error on client-side, after component mounts
+  useEffect(() => {
+    setThrowError(true);
+  }, []);
+
+  if (throwError) {
+    throw new Error("This is a test error!");
+  }
+
+  return <div>Component loaded safely. ErrorBoundary can catch errors.</div>;
 };
 
 export default ErrorProneComponent;
